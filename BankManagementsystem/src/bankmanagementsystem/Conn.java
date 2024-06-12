b4d276bacc7aa388d6b510f2f12847220e42ee1f
harshali dandge <harshalidandge0910@gmail.com>
2024-06-12 14:42:28 +0700

    my first project

---
+++ BankManagementsystem/src/bankmanagementsystem/Conn.java
@@ -0,0 +1,25 @@
+package bankmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+
+public class Conn {
+	
+		Connection connection;
+		Statement statement;
+		public Conn() {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","Harshali@123");
+			statement=connection.createStatement();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
